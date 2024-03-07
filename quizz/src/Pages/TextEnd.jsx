19da7db5b471991b098464a37763b5640e418566
jay gupta <jaygupta5474@gmail.com>
2024-03-07 12:31:24 +0530

    completed

--- quizz/src/Pages/TextEnd.jsx
+++ quizz/src/Pages/TextEnd.jsx
@@ -4,6 +4,7 @@ function TextEnd() {
   return (
     <div className='container pt-5'>
       <h1 className='d-flex justify-content-center align-items-center bg-success text-light mt-5 py-5'>Your Test Has Been Submitted</h1>
+      <h3>Your Result will Be Updated Soon...</h3>
     </div>
   )
 }
